2ac9cdc78cdd0c6f6ba0c32118d819f3bb71f8f6
Joshua <joshuajessy47@gmail.com>
2026-05-26 23:43:19 +0200

    fixed blog scrol

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -2,7 +2,6 @@
 	import BlogCard from '$lib/components/blog/BlogCard.svelte';
 	import CtaSection from '$lib/components/homepage/CtaSection.svelte';
 	import PageHero from '$lib/components/ui/PageHero.svelte';
-	import { inview } from '$lib/actions/inview';
 	import { JsonLd } from 'svelte-meta-tags';
 
 	let { data } = $props();
@@ -28,10 +27,7 @@
 		</p>
 	</div>
 {:else}
-	<div
-		use:inview
-		class="mx-auto max-w-7xl px-4 pb-16 opacity-0 translate-y-4 transition-all duration-700 ease-out [&.in-view]:opacity-100 [&.in-view]:translate-y-0"
-	>
+	<div class="mx-auto max-w-7xl px-4 pb-16">
 		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 			{#each data.posts as post}
 				<BlogCard {post} />
